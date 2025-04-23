623d2aa37b9381888a9a97dc8328c3b2ff7ab604
PrizeAITech <info@prizetech.xyz>
2025-04-23 13:41:36 +0930

    Updated layout and CSS styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PrizeStakingCalculator from './prizestakingcalculator.jsx';
+import PrizeStakingCalculator from './prizestakingcalculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
